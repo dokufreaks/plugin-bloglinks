17c5c8c3c393523ee6d0f84ddc6c3ebb02a3590f
Mohammad Anini <mohd.anini@gmail.com>
2015-04-22 20:54:48 +0300

    Arabic Translations

---
+++ lang/ar/lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Arabic language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Mohammad Anini <mohd.anini@gmail.com>
+ */
+ 
+$lang['prev_link'] = 'المنشور السابق';
+$lang['next_link'] = 'المنشور التالي';
